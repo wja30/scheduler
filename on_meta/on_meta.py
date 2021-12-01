aef02f32d12f940822f346344b466169e8bbeb5e
Ubuntu <ubuntu@ip-172-31-29-223.ec2.internal>
2021-12-01 04:08:49 +0000

    time evalue, expire 60

--- on_meta/on_meta.py
+++ on_meta/on_meta.py
@@ -49,13 +49,17 @@ def set_meta(r, instype, reqtype, inflight, avglatency, req_sec):
         meta_json = json.dumps(meta_json)
         meta_key = instype + reqtype + "_on" # instype(i1) + reqtype(R) = i1R_on
         r.set(meta_key, meta_json)
-        get_meta(r, meta_key)
+        #get_meta(r, meta_key)
     except Exception as e:
-        logging,info(e)
+        logging.info(e)
+    return meta_key
 
 def on_meta(r, queue):
     # on_meta information update (for window recent 60 seconds)
     inflight = [[0]*5 for j in range(5)]
+    req_sec = [[0]*5 for j in range(5)]
+
+    base_time = time.time()
     for key in r.scan_iter("*-*"):
         #logging.info("keys : " + key)
         get_json = r.get(key)
@@ -67,14 +71,17 @@ def on_meta(r, queue):
 
         # to do : each i1R, i1B, i1G summation is needed
         inflight[ins_index][req_index] = inflight[ins_index][req_index]+1
-        req_sec = 0
+        # count # of request within 60secs
+        
+        try:
+            if (time.time() - float(get_dict["time"]) < float(60)):
+                req_sec[ins_index][req_index] = req_sec[ins_index][req_index]+1
+        except Exception as e:
+            logging.info(e)
+        
         if get_dict["progress"] == 1:
-            set_meta(r, get_dict["endpoint"][-2:], get_dict["reqtype"], inflight[ins_index][req_index], get_dict["latency"], req_sec) 
-
-
-    # all instype + reqtype data initiation
-#    for ins in instype:
- #       for req in reqtype:
+            meta_key = set_meta(r, get_dict["endpoint"][-2:], get_dict["reqtype"], inflight[ins_index][req_index], get_dict["latency"], req_sec[ins_index][req_index]) 
+            get_meta(r, meta_key)
 
 
 def on_meta_main():
