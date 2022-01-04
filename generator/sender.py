57361720782bfbae0bf2c67b7ad20d1c57ded3ce
Ubuntu <ubuntu@ip-172-31-88-95.ec2.internal>
2022-01-04 04:48:49 +0000

    B, loadbalancing : 변형mael (autoins : wait*(0.8)^n), cortex i1 bug fix -> wait*waitlstm (10*delta tuning), autoscaling : 4.1dispatcher 5개 프로세스

--- generator/sender.py
+++ generator/sender.py
@@ -24,7 +24,7 @@ number_reqs = 0
 url = "https://wja300-cortex.s3.amazonaws.com/sound-classifier/mia.wav"
 endpoint = "http://34.233.80.127/call/"
 reqtype = ["R", "B", "G", "Y", "S"]
-reqratio = [100, 0, 0, 0, 0] # req ratio (R : B : G : Y : S)
+reqratio = [0, 100, 0, 0, 0] # req ratio (R : B : G : Y : S)
 #endpoint_check = "http://34.233.80.127/check"
 headers = {"content-type": "application/json"}
 headers_binary = {"content-type": "application/octet-stream"}
@@ -102,7 +102,7 @@ def send_data(timeout, reader):
         # tweet min : 1
         # tweet max : 91113
         # 1/3 정도 수준으로 감소 시키면 적정함 
-        num = int(int(row['tweets']) * 2)
+        num = int(int(row['tweets']) * 0.198)
         num1 = int(row['tweets'])
         print(f'row[tweets] : {num1}')
         print(f'num : {num}')
@@ -127,5 +127,5 @@ def send_data(timeout, reader):
 
 with open(f'./tweet_load_10:30-12:00.csv', 'r') as f:
     reader = csv.DictReader(f)
-    send_data(91,reader)
+    send_data(66,reader)
 
