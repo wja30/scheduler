b48213ba1c61a190a72e301df8b2ca770b60d460
Ubuntu <ubuntu@ip-172-31-29-223.ec2.internal>
2022-07-24 05:46:37 +0000

    fix the bug for extract total reqs accrately

--- exe.sh
+++ exe.sh
@@ -1,7 +1,7 @@
 for ((i=0;i<15;i++)); do
 	nohup python dispatcher/dispatcher.py &
 done
-for ((i=0;i<10;i++)); do
+for ((i=0;i<1;i++)); do # 2022.7.24:10, change to 1 (if 10, total reqs count be overlapped)
 	nohup python on_meta/on_meta.py &
 done
 
